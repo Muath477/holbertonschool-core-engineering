8e8a8217f95a2f32de5bb0c72561ecf3c333643a
Muath Al-Mutairi <moathalmuatiri@gmail.com>
2026-09-16 01:36:42 +0300

    Add update_dictionary function to modify dictionaries
    This module provides a function to update or add key/value pairs in a dictionary.

---
+++ python_fundamentals/core_data_structures/update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that updates or adds a key/value pair in a dictionary."""
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Set a_dictionary[key] to value and return the dictionary."""
+    a_dictionary[key] = value
+    return a_dictionary
