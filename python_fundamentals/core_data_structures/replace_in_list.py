e43b17bdfa129172a5fba4f7cba78cacf6361fe0
Muath Al-Mutairi <moathalmuatiri@gmail.com>
2026-09-16 01:18:25 +0300

    Add docstring to replace_in_list function
    Add a docstring for the replace_in_list function.

--- python_fundamentals/core_data_structures/replace_in_list.py
+++ python_fundamentals/core_data_structures/replace_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module that replaces an element of a list at a specific position."""
 
+
 def replace_in_list(my_list, idx, element):
     """Replace the element at idx with element; return list unchanged
     if idx is negative or out of range.
