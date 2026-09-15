621577a6a435da7b9c02948a5097db2dc48726ad
Muath Al-Mutairi <moathalmuatiri@gmail.com>
2026-09-15 23:08:34 +0300

    Add uppercase function to print strings in uppercase
    This module defines a function that converts a given string to uppercase and prints it.

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module that prints a string in uppercase."""
+
+
+def uppercase(str):
+    """Print str in uppercase (ASCII conversion), followed by a newline."""
+    result = ""
+    for c in str:
+        if 'a' <= c <= 'z':
+            result += chr(ord(c) - 32)
+        else:
+            result += c
+    print(result)
